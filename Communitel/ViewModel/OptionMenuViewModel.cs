6b4a5ebd7f174eb4f35ba752b740ee52a8cf886e
pdrokmargo <pdro_kmargo@hotmail.com>
2016-12-05 23:29:10 -0500

    changes

--- Communitel/ViewModel/OptionMenuViewModel.cs
+++ Communitel/ViewModel/OptionMenuViewModel.cs
@@ -5,15 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using Communitel.Models;
 using System.Windows.Controls;
+using Communitel.Views;
 
 namespace Communitel.ViewModel
 {
     public class OptionMenuViewModel
     {
+        Users userview;
         public OptionMenuViewModel()
         {
-            
+            userview = new Users();
         }
+
+        public UserControl activeView
+        {
+            get;
+            set;
+        }
+
         public List<Grid> Menu { get; set; }
+
+
+
     }
 }
