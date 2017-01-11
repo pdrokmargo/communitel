fa823128fee815d6ba3feb874287caddebee2679
cvisbal0724 <cvisbal0724@gmail.com>
2017-01-11 15:12:34 -0500

    cambios checkin

--- Communitel.CheckIn/ViewModels/CheckInViewModel.cs
+++ Communitel.CheckIn/ViewModels/CheckInViewModel.cs
@@ -10,6 +10,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Communitel.CheckIn.Models;
+using Communitel.Common.Video;
+using AForge.Video.DirectShow;
+using System.Drawing;
 
 //using Windows.Media.Capture;
 //using Windows.Storage;
@@ -25,6 +28,8 @@ namespace Communitel.CheckIn.ViewModels
         public DelegateCommand OpenAddPersonCommand { get; set; }
         public DelegateCommand OpenAddDocumentCommand { get; set; }
         public DelegateCommand OpenAddBagCommand { get; set; }
+        public DelegateCommand LoadCameraCaptureCommand { get; set; }
+        public DelegateCommand OpenCameraCaptureCommand { get; set; }
         [ImportingConstructor]
         public CheckInViewModel()
         {
@@ -32,6 +37,8 @@ namespace Communitel.CheckIn.ViewModels
             OpenAddPersonCommand = new DelegateCommand(OpenAddPersonExecute);
             OpenAddDocumentCommand = new DelegateCommand(OpenAddDocumentExecute);
             OpenAddBagCommand = new DelegateCommand(OpenAddBagExecute);
+            LoadCameraCaptureCommand = new DelegateCommand(LoadCameraCaptureExecure);
+            OpenCameraCaptureCommand = new DelegateCommand(OpenCameraCaptureExecute);
             _documents = new ObservableCollection<Document>();
             _persons = new ObservableCollection<Person>();
             _bags = new ObservableCollection<Bag>();
@@ -61,6 +68,15 @@ namespace Communitel.CheckIn.ViewModels
         private Document _document;
         private Person _person;
         private Bag _bag;
+        /// <summary>
+        /// List of media information device available.
+        /// </summary>
+        private IEnumerable<MediaInformation> _mediaDeviceList;
+        private MediaInformation _selectedVideoDevice;
+        /// <summary>
+        /// Byte array of snapshot image.
+        /// </summary>
+        private Bitmap _snapshotBitmap;
         #endregion
 
         #region properties
@@ -72,7 +88,25 @@ namespace Communitel.CheckIn.ViewModels
         public ObservableCollection<Models.Document> Documents { get { return _documents; } set { _documents = value; NotifyPropertyChanged("Documents"); } }
         public ObservableCollection<Models.Person> Persons { get { return _persons; } set { _persons = value; NotifyPropertyChanged("Persons"); } }
         public ObservableCollection<Models.Bag> Bags { get { return _bags; } set { _bags = value; NotifyPropertyChanged("Bags"); } }
+        /// <summary>
+        /// Gets or sets media device list.
+        /// </summary>
+        public IEnumerable<MediaInformation> MediaDeviceList { get { return this._mediaDeviceList; } set { this._mediaDeviceList = value; NotifyPropertyChanged("MediaDeviceList"); } }
+        /// <summary>
+        /// Gets or sets selected media video device.
+        /// </summary>
+        public MediaInformation SelectedVideoDevice { get { return this._selectedVideoDevice; } set { this._selectedVideoDevice = value; NotifyPropertyChanged("SelectedVideoDevice"); } }
+        /// <summary>
+        /// Gets or sets snapshot bitmap.
+        /// </summary>
+        public Bitmap SnapshotBitmap { get { return this._snapshotBitmap; } set { this._snapshotBitmap = value; NotifyPropertyChanged("SnapshotBitmap"); } }
         #endregion
+
+        private void LoadCameraCaptureExecure()
+        {
+            var filterVideoDeviceCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            this.MediaDeviceList = (from FilterInfo filterInfo in filterVideoDeviceCollection select new MediaInformation { DisplayName = filterInfo.Name, UsbId = filterInfo.MonikerString }).ToList();
+        }
 
         private void NextOrPreviousExecute(string view)
         {
@@ -125,8 +159,22 @@ namespace Communitel.CheckIn.ViewModels
             }
         }
 
+        private void OpenCameraCaptureExecute()
+        {
+            try
+            {
+                OpenPopupModal("Camera");
+                RegionManager.RequestNavigate(RegionNames.ContentModalRegion, "/CameraCaptureView");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Common.Enums.MessageBoxIconV.Error);
+            }
+        }
+
         /// <summary>
         /// https://msdn.microsoft.com/en-us/windows/uwp/audio-video-camera/capture-photos-and-video-with-cameracaptureui
+        /// https://blog.jsinh.in/preview-webcam-video-and-take-snapshot-in-wpf-using-aforge-and-mvvm/#.WHZoyBt97IU
         /// </summary>
         private async void StarCaptureExecute()
         {
---
+++ Communitel.Common/Converts/ConvertToImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Communitel.Common.Converts
+{
+    public class ConvertToImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Bitmap)
+            {
+                var bitmap = (Bitmap)value;
+                var imageSourceConverter = new ImageSourceConverter();
+                using (var memoryStream = new MemoryStream())
+                {
+                    bitmap.Save(memoryStream, ImageFormat.Png);
+                    var snapshotBytes = memoryStream.ToArray();
+                    return (ImageSource)imageSourceConverter.ConvertFrom(snapshotBytes); ;
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
